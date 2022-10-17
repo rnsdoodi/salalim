0485a6ae81645011c0c6e60efb6caea58c2e453a
rnsdo <rnsdoodi9@gmail.com>
2022-10-17 21:03:25 +0300

    choice (main.py) update

--- main.py
+++ main.py
@@ -309,6 +309,9 @@ def choice(cvs_id):
             db.session.commit()
             all_users.append(new_user)
             flash(f" 0{new_user.Contact} تم الاختيار بنجاح وسوف نقوم بالتواصل معكم على الرقم ")
+
+        if cv_to_select == None:
+            flash("العاملة محجوزة مسبقاً, فضلاً قم بتحديث الصفحة والاختيار من جديد ")
         else:
             flash("لقد قمت بإدخال رقم تعريف خاطئ , الرجاء التأكد من رقم التعريف والمحاولة مرة أخرى ")
         return redirect(url_for('philippines'))
